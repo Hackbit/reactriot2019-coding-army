b222f8ebeb6c0c0e6862751d12bd2a9cab141b7c
Reena Bano <reenaishtiaq005@gmail.com>
2019-09-28 12:04:51 +0500

    created components

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Nav extends Component {
+    render(props) {
+        return (
+            <div>
+                
+                
+            </div>
+        )
+    }
+}
