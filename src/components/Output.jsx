ca6c34ab96a3804e6d374b34b5f48d3f7700d390
Somia Sohail Ansari <ansarisomia@gmail.com>
2019-09-28 18:11:43 +0500

    wip- css of output

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MaleCartoon from './MaleCartoon';
+import Bubble from './Bubble';
 import './Output.css';
 
 export default function Output() {
   return (
     <div className="output">
+      <Bubble />
       <MaleCartoon />
     </div>
   )
