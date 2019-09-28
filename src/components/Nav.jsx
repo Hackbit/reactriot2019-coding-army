1bfac1d6f32815ba311b0aabca957869f54be7b4
Reena Bano <reenaishtiaq005@gmail.com>
2019-09-28 13:35:35 +0500

    Adeed nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 
 export default class Nav extends Component {
-    render(props) {
+    render() {
         return (
-            <div>
-                
-                
-            </div>
+            <header>
+                <nav className="homepg-nav">
+
+                    <ul className="list-container">
+                        <a className="link" href="google.com"><li className="list-item"><i class="fas fa-home"></i> Home</li></a>
+                        <a className="link" href="google.com"><li className="list-item"><i class="far fa-check-circle"></i> Vote for us</li></a>
+                        <a className="link" href="google.com"><li className="list-item"><i class="far fa-thumbs-up"></i> Like our repo</li></a>
+                        <a className="link" href="google.com"><li className="list-item"><i class="fas fa-users"></i> Follow us</li></a>
+                    </ul>
+                </nav>
+            </header>
+
+
+
         )
     }
 }
