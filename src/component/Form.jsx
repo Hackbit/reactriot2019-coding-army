414a4749760659751363f02d2a2d1f5f15119251
Niya Rukhman <niyarukhman123@gmail.com>
2019-09-28 23:16:54 +0500

    added code again

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,56 +1,47 @@
-import React from 'react';
-import './Form.css';
-import { CirclePicker } from 'react-color';
-
+import React from "react";
+import "./Form.css";
+import { CirclePicker } from "react-color";
 
 function handleChange(color, event) {
-    console.log(color);
+  console.log(color);
 }
-function Form (){
-    
-    return (
-            <form>
+function Form() {
+  return (
+    <form>
+      <h1>CARTOONIFY GAME FORM</h1>
 
-                
-                    <h1>CARTOONIFY GAME FORM</h1>
-               
-                <div className="main">
-                    <label className="name">Full Name </label>
-                    <br />
-                    <input type="text" className="nametwo" placeholder="Your good name here..."></input>
-                    <br />
-                    <label className="gender">Choose Your Gender </label>
-                    <br />
-                    <input type="radio" name="gender" value="male" /> Male
-                    <br />
-                    <input type="radio" name="gender" value="female" /> Female
-                    <br />
-                    <label className="color">Select Your Favorite Color </label>
-                    <CirclePicker className="colorChoose" onChange={handleChange} />
-                    <br />
-                    <label className="place">Which Is Your Favorite Place</label>
-                    <br /> 
-                    <select>
-                                <option value="mountain">Mountain</option>
-                                <option value="beeach">Beach</option>
-                                <option value="desert">Desert</option>
-                                <option value="valley">Valley</option>
-                                <option value="forest">Forest</option>
-                    </select>
-                            <br />
-                            
-                            <input className="submit" type="submit"></input>
-                           
-                            
-                            
-</div>
-            </form>
-               
-                 );
-            
-        
-    }
+      <div className="main">
+        <label className="name">Full Name </label>
+        <br />
+        <input
+          type="text"
+          className="nametwo"
+          placeholder="Your good name here..."
+        ></input>
+        <br />
+        <label className="gender">Choose Your Gender </label>
+        <br />
+        <input type="radio" name="gender" value="male" /> Male
+        <br />
+        <input type="radio" name="gender" value="female" /> Female
+        <br />
+        <label className="color">Select Your Favorite Color </label>
+        <CirclePicker className="colorChoose" onChange={handleChange} />
+        <br />
+        <label className="place">Which Is Your Favorite Place</label>
+        <br />
+        <select>
+          <option value="mountain">Mountain</option>
+          <option value="beeach">Beach</option>
+          <option value="desert">Desert</option>
+          <option value="valley">Valley</option>
+          <option value="forest">Forest</option>
+        </select>
+        <br />
+        <input className="submit" type="submit"></input>
+      </div>
+    </form>
+  );
+}
 
-        
-        
 export default Form;
