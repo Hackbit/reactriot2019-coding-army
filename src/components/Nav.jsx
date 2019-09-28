8bd1faaf37961c95260138bfe3594e7eb6908071
Somia Sohail Ansari <ansarisomia@gmail.com>
2019-09-28 18:48:09 +0500

    indent

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,14 +5,14 @@ export default class Nav extends Component {
     return (
       <header>
         <nav className="homepg-nav">
-                    <ul className="list-container">
-                        <a className="link" href="google.com"><i class="fas fa-home"></i> Home</a>
-                        <a className="link" href="google.com"><i class="far fa-check-circle"></i> Vote for us</a>
-                        <a className="link" href="google.com"><i class="far fa-thumbs-up"></i> Like our repo</a>
-                        <a className="link" href="google.com"><i class="fas fa-users"></i> Follow us</a>
-                    </ul>
-                </nav>
-            </header>
-        )
-    }
+          <ul className="list-container">
+            <a className="link" href="google.com"><i class="fas fa-home"></i> Home</a>
+            <a className="link" href="google.com"><i class="far fa-check-circle"></i> Vote for us</a>
+            <a className="link" href="google.com"><i class="far fa-thumbs-up"></i> Like our repo</a>
+            <a className="link" href="google.com"><i class="fas fa-users"></i> Follow us</a>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
 }
