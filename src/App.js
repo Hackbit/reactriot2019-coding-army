370ab3ecf45c57aaf99080bb17ceb60bee38aaff
Somia Sohail Ansari <ansarisomia@gmail.com>
2019-09-29 10:09:04 +0500

    onclick show form

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
     <div className="App">
       <Nav />
       <LetsPlay />
-      <Output />
     </div>
   );
 }
--- src/components/LetsPlay.jsx
+++ src/components/LetsPlay.jsx
@@ -7,30 +7,27 @@ export default class LetsPlay extends Component {
     shown: true,
   };
 
-  handleClick() {
+  toggleShown = () => {
+    this.setState({ shown: !this.state.shown })
+  }
+  render() {
+    const { shown } = this.state;
+
     return (
       <div>
-        <Form />
-        {console.log('this is working')}
+        {
+          shown ? (
+            <div className="LetsPlay">
+              <h1 className="main-heading"> Welcome To Cartoonify Game </h1>
+              <h3 className="sub-heading">Let's play! Submit your answer to the following questions</h3>
+              <button className="play-game"
+                onClick={this.toggleShown}>PLAY</button>
+            </div >
+          ) : (
+              <Form />
+            )
+        }
       </div>
     )
-  }
-  display() { this.setState({ shown: !this.state.shown }) }
-
-  render() {
-    return (
-      <div className="LetsPlay"
-        style={{ display: this.state.shown ? 'true' : 'none' }}>
-        <h1 className="main-heading"> Welcome To Cartoonify Game </h1>
-        <h3 className="sub-heading">Let's play! Submit your answer to the following questions</h3>
-        <button className="play-game"
-          onClick={(event) => { this.display(); this.handleClick(); }}
-        >
-          PLAY
-        </button>
-      </div >
-    )
-
   };
-
 }
