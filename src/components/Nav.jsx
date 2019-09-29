34a2122d1afd624b6e8a457c3629ca901c49cc08
Reena Bano <reenaishtiaq005@gmail.com>
2019-09-29 18:03:28 +0500

    Added fevicon

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,7 +15,7 @@ export default class Nav extends Component {
                             <a className="link" href="google.com"><i className="fas fa-home"></i> Home</a>
                             <a className="link" href="google.com"><i className="fas fa-check-circle"></i> Vote for us</a>
                             <a className="link" href="google.com"><i className="fas fa-thumbs-up"></i> Like our repo</a>
-                            <a className="link" href="google.com"><i className="fas fa-users"></i> Follow us</a>
+                            <a className="link" href="https://github.com/reenaishtiaq"><i className="fas fa-users"></i> Follow us</a>
                         </ul>
                         <div className="logo">
                             <img src={logo} alt="logo" width="100px" height="80px" />;
