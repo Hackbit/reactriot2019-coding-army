3301d800eb4339ed2d901011d246b57e5f81ff9d
Reena Bano <reenaishtiaq005@gmail.com>
2019-09-29 18:49:48 +0500

    Updates

---
+++ precache-manifest.40d743d3b75e1e88d3dfdbd497958886.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ecca1766b074acdd9e6d6056377ad2",
+    "url": "/TD-React-Challenge-Test-1/index.html"
+  },
+  {
+    "revision": "9c6f8d9b9f38ca4a765c",
+    "url": "/TD-React-Challenge-Test-1/static/css/main.e92d6542.chunk.css"
+  },
+  {
+    "revision": "fe47b49f856b92bf5a2b",
+    "url": "/TD-React-Challenge-Test-1/static/js/2.716ecf86.chunk.js"
+  },
+  {
+    "revision": "9c6f8d9b9f38ca4a765c",
+    "url": "/TD-React-Challenge-Test-1/static/js/main.22650846.chunk.js"
+  },
+  {
+    "revision": "1b88777c4e1c5b595da6",
+    "url": "/TD-React-Challenge-Test-1/static/js/runtime-main.077a338e.js"
+  },
+  {
+    "revision": "285247f8e6e16d0b45dd119f1b55050f",
+    "url": "/TD-React-Challenge-Test-1/static/media/beach.285247f8.jpg"
+  },
+  {
+    "revision": "7c7dcdfdf91cda0c13421f0afc17daad",
+    "url": "/TD-React-Challenge-Test-1/static/media/capture1.7c7dcdfd.PNG"
+  },
+  {
+    "revision": "9ad61bbad468d8a9419ec59c680a7ac2",
+    "url": "/TD-React-Challenge-Test-1/static/media/logo.9ad61bba.png"
+  }
+]);
