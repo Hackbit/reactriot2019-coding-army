e2e98f85e2d30e46db32663da8e8b33d5b33b95c
Somia Sohail Ansari <ansarisomia@gmail.com>
2019-09-30 00:03:57 +0500

    update links in nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,12 +11,12 @@ export default class Nav extends Component {
             <>
                 <header>
                     <nav className="homepg-nav">
-                        <ul className="list-container">
-                            <a className="link" href="google.com"><i className="fas fa-home"></i> Home</a>
-                            <a className="link" href="google.com"><i className="fas fa-check-circle"></i> Vote for us</a>
-                            <a className="link" href="google.com"><i className="fas fa-thumbs-up"></i> Like our repo</a>
-                            <a className="link" href="https://github.com/reenaishtiaq"><i className="fas fa-users"></i> Follow us</a>
-                        </ul>
+                        <ul class="list-container">
+
+                            <a class="link" href="https://hackbit.github.io/reactriot2019-coding-army/."><i class="fas fa-home"></i> Home</a>
+                            <a class="link" href="https://www.reactriot.com/entries/74-coding-army/vote"><i class="fas fa-check-circle"></i> Vote for us</a>
+                            <a class="link" href="https://www.reactriot.com/entries/74-coding-army/vote"><i class="fas fa-thumbs-up"></i> Like our repo</a>
+                            <a class="link" href="https://github.com/reenaishtiaq"><i class="fas fa-users"></i> Follow us</a></ul>
                         <div className="logo">
                             <img src={logo} alt="logo" width="100px" height="80px" />;
                         </div>
